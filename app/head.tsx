30c5299654a0de9824808735dad527c4aa5b3b99
tomas <tomasezequiel.benitez@gmail.com>
2023-08-16 16:44:09 -0300

    update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -12,21 +12,21 @@ export default function Head() {
       <title>{meta.title}</title>
       <meta name='robots' content='all' />
       <meta content={meta.description} name='description' />
-      <meta property='og:url' content='https://photography.vercel.app' />
-      <link rel='canonical' href='https://photography.vercel.app' />
+      <meta property='og:url' content='https://photography-neon.vercel.app' />
+      <link rel='canonical' href='https://photography-neon.vercel.app' />
       <meta property='og:type' content={meta.type} />
       <meta property='og:site_name' content={meta.title} />
       <meta property='og:description' content={meta.description} />
       <meta property='og:title' content={meta.title} />
       <meta
         property='og:image'
-        content={`https://photography.vercel.app${meta.image}`}
+        content={`https://photography-neon.vercel.app${meta.image}`}
       />
       <meta property='og:image:width' content='1920' />
       <meta property='og:image:height' content='1080' />
       <meta
         property='twitter:image'
-        content={`https://photography.vercel.app${meta.image}`}
+        content={`https://photography-neon.vercel.app${meta.image}`}
       />
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='robots' content='all' />
